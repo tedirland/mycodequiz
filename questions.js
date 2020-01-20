1ec1c8b602826bb3e41d0fe7309cb62ad588af28
Ted Irland <irlandth@gmail.com>
2020-01-20 17:43:31 -0600

    Initial commit

---
+++ questions.js
@@ -0,0 +1,20 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+   
+  ];
+
+  var score=0;
+
+  for (var i = 0; i<questions.length; i++) {
+   
+  }
+
